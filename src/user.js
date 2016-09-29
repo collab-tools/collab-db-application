c598cc8951b6668354ef448dec369ce568c3de6b
Yeap Hooi Tong <hooitongx@gmail.com>
2016-09-29 15:47:08 +0800

    Removed leftover debug messages

--- src/user.js
+++ src/user.js
@@ -23,7 +23,6 @@ module.exports = function (sequelize, DataTypes) {
         return this.findById(id);
       },
       getUsers() {
-        console.log(this);
         return this.findAll();
       },
       getUsersCount() {
